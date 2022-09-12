c398bd0937440a86528887bfdc746fd1c106dc0d
Brandon-Smith <bcsmith2@ncsu.edu>
2022-09-11 20:32:15 -0400

    minor update to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='bitsight',
     version='0.2.1',
@@ -9,6 +13,8 @@ setup(
     author='Brandon Smith',
     author_email='bcsmith2@gmail.com',
     description='Unofficial Package for BitSight\'s API',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     python_requires='>=3.7',
     install_requires=[
         'requests>=2.28.1',
