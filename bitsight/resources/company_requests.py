284f052cdb186301407177372249302cd3421fc1
brandon <bcsmith2@gmail.com>
2025-09-02 21:24:39 -0400

    added optional api_key param to CompanyRequests and type hints to methods

--- bitsight/resources/company_requests.py
+++ bitsight/resources/company_requests.py
@@ -1,13 +1,13 @@
-from bitsight.resources.bitsight import BitSight, Endpoints
+from bitsight.resources.bitsight import BitSight, Endpoints, QueryParams, LicenseType
 
 
 class CompanyRequests(BitSight):
     v2_endpoint = f"{Endpoints.V2.company_requests}"
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, api_key: str | None = None):
+        super().__init__(api_key)
 
-    def get_request_status(self, guid, params=None, **kwargs):
+    def get_request_status(self, guid: str, params: QueryParams = None, **kwargs):
         """
         Get the status of a request
         :param guid: the guid for the request
@@ -17,7 +17,7 @@ class CompanyRequests(BitSight):
 
         return self.get(endpoint=self.v2_endpoint + guid, params=params, **kwargs)
 
-    def get_all_company_requests(self, params=None, **kwargs):
+    def get_all_company_requests(self, params: QueryParams = None, **kwargs):
         """
         Get details on all company requests
         :param params: filters for the request
@@ -26,7 +26,9 @@ class CompanyRequests(BitSight):
 
         return self.get(endpoint=self.v2_endpoint, params=params, **kwargs)
 
-    def post_request_company(self, domain, subscription_type=None, **kwargs):
+    def post_request_company(
+        self, domain: str, subscription_type: str | LicenseType | None = None, **kwargs
+    ):
         """
         Request to subscribe to a company in BitSight
         :param subscription_type: the license type to use to subscribe when the company is available
